b7c6aa3adba48f8831ef9fb35b58c31951f88e5b
h-dychko <galiadychko@outlook.com>
2023-12-06 19:04:06 +0200

    initial

---
+++ app.py
@@ -0,0 +1,212 @@
+import streamlit as st
+import uuid
+import sys
+import os
+import re
+
+import kendra_chat_bedrock_claudev2 as bedrock_claudev2
+import kendra_chat as llmchat
+from dotenv import load_dotenv
+
+
+USER_ICON = "images/user-icon.png"
+AI_ICON = "images/ai-icon.jpg"
+MAX_HISTORY_LENGTH = 5
+
+dotenv_path = os.path.join(os.path.join(os.path.dirname(__file__), '.streamlit'), "config.toml")
+load_dotenv(dotenv_path)
+
+SOURCES_DICT = {
+  f"https://{os.environ['S3_BUCKET']}.s3.amazonaws.com/Internal%20Audit%20LMO%20June%202023.pptx": {
+    "link": f"https://{os.environ['S3_BUCKET']}.s3.amazonaws.com/Internal%20Audit%20LMO%20June%202023.pptx?AWSAccessKeyId={os.environ['AWSAccessKeyId']}&Signature=9H9obJLIlDN8h1A5loU35toqipY%3D&Expires=1702482499",
+    "file_name": "Internal Audit LMO June 2023.pptx"
+  },
+  f"https://{os.environ['S3_BUCKET']}.s3.amazonaws.com/LMO-FOCUS-Presentation_Oct-2023.pptx": {
+    "link": f"https://{os.environ['S3_BUCKET']}.s3.amazonaws.com/LMO-FOCUS-Presentation_Oct-2023.pptx?AWSAccessKeyId={os.environ['AWSAccessKeyId']}&Signature=wf6uQLxEt1L9d%2F9nJnWwtrsttBs%3D&Expires=1702482582",
+    "file_name": "LMO-FOCUS-Presentation_Oct-2023.pptx"
+    },
+  f"https://{os.environ['S3_BUCKET']}.s3.amazonaws.com/Risk%20%26%20Insurance%20LMO%202023.pptx": {
+    "link": f"https://{os.environ['S3_BUCKET']}.s3.amazonaws.com/Risk%20%26%20Insurance%20LMO%202023.pptx",
+    "file_name": "Risk & Insurance LMO 2023.pptx"
+  },
+  f"https://{os.environ['S3_BUCKET']}.s3.amazonaws.com/Payroll_HRIS+LMO_2023_+.pptx": {
+    "link": f"https://{os.environ['S3_BUCKET']}.s3.amazonaws.com/Payroll_HRIS%20LMO_2023_%20.pptx?AWSAccessKeyId={os.environ['AWSAccessKeyId']}&Signature=tld2ByoZViqZgk%2BqVOF%2B%2F1pzE6g%3D&Expires=1702482647",
+    "file_name": "Payroll_HRIS LMO_2023_ .pptx"
+  },
+  f"https://{os.environ['S3_BUCKET']}.s3.amazonaws.com/Procurement%20LMO%202023%20Rev%20A%20JRM.pptx": {
+    "link": f"https://{os.environ['S3_BUCKET']}.s3.amazonaws.com/Procurement%20LMO%202023%20Rev%20A%20JRM.pptx?AWSAccessKeyId={os.environ['AWSAccessKeyId']}&Signature=uDNd9Eyq2%2BInBdpJQvXhY8bbPbc%3D&Expires=1702484661",
+    "file_name": "Procurement LMO 2023 Rev A JRM.pptx"
+  }
+}
+
+# Check if the user ID is already stored in the session state
+if 'user_id' in st.session_state:
+    user_id = st.session_state['user_id']
+
+# If the user ID is not yet stored in the session state, generate a random UUID
+else:
+    user_id = str(uuid.uuid4())
+    st.session_state['user_id'] = user_id
+
+
+if 'llm_chain' not in st.session_state:
+    if (len(sys.argv) > 1):
+        if (sys.argv[1] == 'bedrock_claudev2'):
+            st.session_state['llm_app'] = bedrock_claudev2
+            st.session_state['llm_chain'] = bedrock_claudev2.build_chain()
+        else:
+            raise Exception("Unsupported LLM: ", sys.argv[1])
+    else:
+        raise Exception("Usage: streamlit run app.py <bedrock_claudev2>")
+
+if 'chat_history' not in st.session_state:
+    st.session_state['chat_history'] = []
+    
+if "chats" not in st.session_state:
+    st.session_state.chats = [
+        {
+            'id': 0,
+            'question': '',
+            'answer': ''
+        }
+    ]
+
+if "questions" not in st.session_state:
+    st.session_state.questions = []
+
+if "answers" not in st.session_state:
+    st.session_state.answers = []
+
+if "input" not in st.session_state:
+    st.session_state.input = ""
+
+
+st.markdown("""
+        <style>
+               .block-container {
+                    padding-top: 32px;
+                    padding-bottom: 32px;
+                    padding-left: 0;
+                    padding-right: 0;
+                }
+                .element-container img {
+                    background-color: #000000;
+                }
+
+                .main-header {
+                    font-size: 24px;
+                }
+        </style>
+        """, unsafe_allow_html=True)
+
+def write_logo():
+    col1, col2, col3 = st.columns([5, 1, 5])
+    with col2:
+        st.image(AI_ICON, use_column_width='always') 
+
+
+def write_top_bar():
+    col1, col2, col3 = st.columns([1,10,2])
+    with col1:
+        st.image(AI_ICON, use_column_width='always')
+    with col2:
+        selected_provider = sys.argv[1]
+        provider = selected_provider.capitalize()
+        header = f"Hi, it's your onboarding assistant!"
+        st.write(f"<h2 class='main-header'>{header}</h2>", unsafe_allow_html=True)
+    with col3:
+        clear = st.button("Clear Chat")
+    return clear
+
+clear = write_top_bar()
+
+if clear:
+    st.session_state.questions = []
+    st.session_state.answers = []
+    st.session_state.input = ""
+    st.session_state["chat_history"] = []
+
+def handle_input():
+    input = st.session_state.input
+    question_with_id = {
+        'question': input,
+        'id': len(st.session_state.questions)
+    }
+    st.session_state.questions.append(question_with_id)
+
+    chat_history = st.session_state["chat_history"]
+    if len(chat_history) == MAX_HISTORY_LENGTH:
+        chat_history = chat_history[:-1]
+
+    llm_chain = st.session_state['llm_chain']
+    chain = st.session_state['llm_app']
+    result = chain.run_chain(llm_chain, input, chat_history)
+
+    result["answer"] = re.sub("<knowledge>", "", result["answer"])
+    result["answer"] = re.sub("</knowledge>", "",  result["answer"])
+
+    answer = result['answer']
+    chat_history.append((input, answer))
+    
+    document_list = []
+    if 'source_documents' in result:
+        for d in result['source_documents']:
+            if not (d.metadata['source'] in document_list):
+                document_list.append((d.metadata['source']))
+
+    st.session_state.answers.append({
+        'answer': result,
+        'sources': document_list,
+        'id': len(st.session_state.questions)
+    })
+    st.session_state.input = ""
+
+def write_user_message(md):
+    col1, col2 = st.columns([1,12])
+    
+    with col1:
+        st.image(USER_ICON, use_column_width='always')
+    with col2:
+        st.warning(md['question'])
+
+
+def render_result(result):
+    answer, sources = st.tabs(['Answer', 'Sources'])
+    with answer:
+        render_answer(result['answer'])
+    with sources:
+        if 'source_documents' in result:
+            render_sources(result['source_documents'])
+        else:
+            render_sources([])
+
+def render_answer(answer):
+    col1, col2 = st.columns([1,12])
+    with col1:
+        st.image(AI_ICON, use_column_width='always')
+    with col2:
+        st.info(answer['answer'])
+
+def render_sources(sources):
+    col1, col2 = st.columns([1,12])
+    with col2:
+        with st.expander("Sources"):
+            for s in sources:
+                st.markdown(f"<a href='{SOURCES_DICT[s]['link']}'>{SOURCES_DICT[s]['file_name']}</a>", unsafe_allow_html=True)
+
+    
+#Each answer will have context of the question asked in order to associate the provided feedback with the respective question
+def write_chat_message(md, q):
+    chat = st.container()
+    with chat:
+        render_answer(md['answer'])
+        render_sources(md['sources'])
+    
+        
+with st.container():
+  for (q, a) in zip(st.session_state.questions, st.session_state.answers):
+    write_user_message(q)
+    write_chat_message(a, q)
+
+st.markdown('---')
+input = st.text_input("It's your an AI-based assistant. How can I help you?", key="input", on_change=handle_input)
---
+++ kendra_chat_bedrock_claudev2.py
@@ -0,0 +1,145 @@
+# from aws_langchain.kendra import AmazonKendraRetriever #custom library
+from langchain.retrievers import AmazonKendraRetriever
+from langchain.chains import ConversationalRetrievalChain
+from langchain.prompts import PromptTemplate
+from langchain.llms.bedrock import Bedrock
+from langchain.chains.llm import LLMChain
+
+import boto3
+from langchain.llms import Bedrock
+
+import sys
+import os
+
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+MAX_HISTORY_LENGTH = 5
+
+def build_chain():
+  region = os.environ["AWS_REGION"]
+  kendra_index_id = os.environ["KENDRA_INDEX_ID"]
+
+  session = boto3.Session(region_name = region)
+  boto3_bedrock = session.client(service_name="bedrock-runtime")
+
+  llm = Bedrock(
+      client=boto3_bedrock,
+      region_name = region,
+      model_kwargs={"max_tokens_to_sample":300,"temperature":1,"top_k":250,"top_p":0.999,"anthropic_version":"bedrock-2023-05-31"},
+      model_id="anthropic.claude-v2:1"
+  )
+
+  retriever = AmazonKendraRetriever(index_id=kendra_index_id,top_k=5,region_name=region)
+
+  prompt_template = """
+  Human: You will be acting as an onboarding assistant for a newcomer location manager.
+
+    You'll receive a newcomer's question and a piece of text from a .pptx presentation to generate an answer to the user's query. 
+
+    Text from a .pptx presentation will be inside the block <knowledge></knowledge>.
+
+    Firstly, you have to generate an answer to the question based on the input information in the block <knowledge></knowledge>.  
+    Secondly, provide the exact truncated quotes with only relevant information for your answer.
+
+    In case the knowledge block doesn't contain an answer to the user's query, write, "I'm sorry, but I couldn't extract the answer from the knowledge base. 
+    The most relevant information I have is <knowledge></knowledge>", 
+    where instead of <knowledge></knowledge> put the information from the <knowledge></knowledge> block.
+
+    You are forbidden to use any additional information except from the knowledge block.
+    You are forbidden to rephrase the same information in one answer for more or equal one time. 
+    You are forbidden to cite yourself in the answer. You're allowed to cite knowledge block only and no more than one time.
+
+    Do not repeat the question in your answer; do not rephrase the question block in your answer.
+    You are forbidden to include intro and summary/conclusion parts and anything except neat and short-spoken information in your answer. 
+    You are forbidden to include synonyms of the phrase 'my answer' with meaning that it's your answer in your answer.
+
+
+  Human: Here are a few pieces of knowledge in <knowledge> tags:
+  <knowledge>
+  {context}
+  </knowledge>
+
+    Based on the above knowledge, provide a short informative answer for {question}. 
+    In case the knowledge block doesn't contain an answer to the user's query, write exactly 
+    "I'm sorry, but I couldn't extract the answer from the knowledge base. The most relevant information I have is <knowledge></knowledge>", 
+    where instead of knowledge put the information from the knowledge block.
+    You are forbidden to use any additional information except from the knowledge block. 
+    
+    Do not repeat the question in your answer; do not rephrase the question block in your answer.
+    You are forbidden to include intro and summary/conclusion parts and anything except neat and short-spoken information in your answer. 
+    You are forbidden to include synonyms of the phrase 'my answer' with meaning that it's your answer in your answer.
+
+
+  Assistant:
+  """
+  PROMPT = PromptTemplate(
+      template=prompt_template, input_variables=["context", "question"]
+  )
+
+  condense_qa_template = """{chat_history}
+  Human:
+  Given the previous conversation and a follow up question below, rephrase the follow up question
+  to be a standalone question.
+
+  Follow Up Question: {question}
+  Standalone Question:
+
+  Assistant:"""
+  standalone_question_prompt = PromptTemplate.from_template(condense_qa_template)
+
+
+  
+  qa = ConversationalRetrievalChain.from_llm(
+        llm=llm, 
+        retriever=retriever, 
+        condense_question_prompt=standalone_question_prompt, 
+        return_source_documents=True, 
+        combine_docs_chain_kwargs={"prompt":PROMPT},
+        verbose=True)
+
+  return qa
+
+
+def run_chain(chain, prompt: str, history=[]):
+  return chain({"question": prompt, "chat_history": history})
+
+
+# if __name__ == "__main__":
+#   chat_history = []
+#   qa = build_chain()
+#   print(bcolors.OKBLUE + "Hello! How can I help you?" + bcolors.ENDC)
+#   print(bcolors.OKCYAN + "Ask a question, start a New search: or CTRL-D to exit." + bcolors.ENDC)
+#   print(">", end=" ", flush=True)
+#   for query in sys.stdin:
+#     if (query.strip().lower().startswith("new search:")):
+#       query = query.strip().lower().replace("new search:","")
+#       chat_history = []
+#     elif (len(chat_history) == MAX_HISTORY_LENGTH):
+#       chat_history.pop(0)
+#     result = run_chain(qa, query, chat_history)
+#     import re
+    
+#     result["answer"] = re.sub("<knowledge>", result["answer"], "")
+#     result["answer"] = re.sub("</knowledge>", result["answer"], "")
+
+#     chat_history.append((query, result["answer"]))
+#     print(bcolors.OKGREEN + result['answer'] + bcolors.ENDC)
+#     if 'source_documents' in result:
+#       print(bcolors.OKGREEN + 'Sources:')
+#       for d in result['source_documents']:
+#         print(d.metadata['source'])
+#     print(bcolors.ENDC)
+#     print(bcolors.OKCYAN + "Ask a question, start a New search: or CTRL-D to exit." + bcolors.ENDC)
+#     print(">", end=" ", flush=True)
+#   print(bcolors.OKBLUE + "Bye" + bcolors.ENDC)
+  
